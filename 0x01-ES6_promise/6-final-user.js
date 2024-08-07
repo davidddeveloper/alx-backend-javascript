240ecf63949d2724f58b4d35b4d8e2495dbbe56f
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-07 13:21:09 +0000

    handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,13 +4,13 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return new Promise(() => {
     const result = [];
-    signUpUser(firstName, lastName).then((response) => {
-      result.push({ status: 'success', value: response });
+    signUpUser(firstName, lastName).then((response, statusText) => {
+      result.push({ status: statusText, value: response });
     }, (response) => {
       result.push(response);
     });
-    uploadPhoto(fileName).then((response) => {
-      result.push({ status: 'failed', value: response });
+    uploadPhoto(fileName).then((response, statusText) => {
+      result.push({ status: statusText, value: response });
     }, () => {});
     return result;
   });
