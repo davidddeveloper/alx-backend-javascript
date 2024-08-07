5324f549247bb4fc70569de5beaa026d10defe39
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-07 14:32:03 +0000

    handling multiple successfull promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((resultArray) => {
-    console.log(resultArray[0].body, resultArray[1].firstName, resultArray[1].lastName);
+  uploadPhoto().then((response) => response.body).then((response) => {
+    const result = response;
+    createUser().then((response) => {
+      console.log(`${result} ${response.firstName} ${response.lastName}`);
+    });
   });
 }
