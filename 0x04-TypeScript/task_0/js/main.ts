df016251c8373b43c2a013f76f5d5e704a88769f
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-15 02:01:14 +0000

    task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const firstStudent: Student = {
+  firstName: 'David',
+  lastName: 'Conteh',
+  age: 20,
+  location: 'Sierra Leone'
+}
+
+const secondStudent: Student = {
+  firstName: 'Bob',
+  lastName: 'Conteh',
+  age: 18,
+  location: 'Sierra Leone'
+}
+
+studentsList = [firstStudent, secondStudent]
