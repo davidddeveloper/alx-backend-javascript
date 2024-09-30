187196f37c97d4b5dbd695156789c5fce5ded6f9
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-30 15:30:19 +0000

    small http server using node's http module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,13 @@
+const {createServer} = require("node:http");
 
+const app = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end("Hello Holberton School!")
+})
+
+app.listen(1245, '127.0.0.1', () => {
+    process.stdout.write("Hello Holberton School!\n")
+})
+
+module.exports = app;
