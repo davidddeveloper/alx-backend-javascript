23eea0853199460a14407edbc4949d7282916a0d
davidddeveloper <programmingdonebydavid@gmail.com>
2024-10-03 22:41:24 +0000

    calculateNumber

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -2,6 +2,4 @@ const calculateNumber = (a, b) => {
   return Math.round(a + b);
 };
 
-console.log(calculateNumber(2.3, 3.5));
-
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe("Test should be around the rounded part", () => {
-  it("should return 5", function () {
+describe('calculateNumber', () => {
+  it('should return 5', function () {
     assert(calculateNumber(2, 3), 5);
   });
 
-  it("should return 5", function () {
+  it('should return 5', function () {
     assert(calculateNumber(1, 3.7), 5);
   });
 
-  it("should be rounded", function () {
+  it('should be rounded', function () {
     assert(calculateNumber(2.3, 3.5), 6);
   });
 
-  it("should return 6", function () {
+  it('should return 6', function () {
     assert(calculateNumber(1.5, 3.7), 6);
   });
 });
