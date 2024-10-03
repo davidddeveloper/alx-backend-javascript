0f6b05da78684254da89708df166b9aa14aeaf94
davidddeveloper <programmingdonebydavid@gmail.com>
2024-10-03 23:55:17 +0000

    2-read-file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
 
+const countStudents = (path) => {
+  try {
+    let data = fs.readFileSync(path, 'utf-8');
+    const sweStudents = [];
+    const csStudents = [];
+
+    data = data.split('\r\n').slice(1);
+    data.forEach((item) => {
+      if (item.endsWith('CS')) {
+        csStudents.push(item);
+      }
+      if (item.endsWith('SWE')) {
+        sweStudents.push(item);
+      }
+    });
+    console.log(`Number of students: ${data.length - 1}`);
+    console.log(data);
+
+    process.stdout.write(`Number of students in CS: ${csStudents.length}. `);
+    process.stdout.write('List: ');
+    csStudents.forEach(student => process.stdout.write(`${student.split(',')[0]} `));
+    console.log();
+    process.stdout.write(`Number of students in SWE: ${sweStudents.length}. `);
+    process.stdout.write('List: ');
+    sweStudents.forEach(student => process.stdout.write(`${student.split(',')[0]} `));
+  } catch (e) {
+    throw Error('Cannot load the database');
+  }
+};
+
+countStudents('database.csv');
