77fec4f8aa8a613fd6702075b48c5a7b1e1c19e7
davidddeveloper <programmingdonebydavid@gmail.com>
2024-10-03 23:10:49 +0000

    handling kill signal correctly

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,11 @@
 process.stdin.on('data', (name) => {
     console.log(`Your name is: ${name}`);
+    process.exit();
 });
 
 console.log(`Welcome to Holberton School, what is your name?`);
 
 process.on('SIGINT', () => {
     console.log("This important software is now closing");
+    process.exit(0);
 });
