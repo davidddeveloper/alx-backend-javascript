9d3bac004c9be7776f2b12e4f0055d48b0c35ba6
davidddeveloper <programmingdonebydavid@gmail.com>
2024-10-04 00:46:28 +0000

    express server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,56 @@
+const { createServer } = require('node:http');
+const fs = require('node:fs');
 
+const databaseFile = process.argv.slice(2)[0];
+
+const app = createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+
+  if (req.url === '/students') {
+    res.write('This is the list of our students');
+    const read = (res) => {
+      fs.readFile(databaseFile, 'utf-8', function (err, data) {
+        if (err) {
+          throw Error('Cannot load the database');
+        }
+
+        const sweStudents = [];
+        const csStudents = [];
+
+        data = data.split('\r\n').slice(1);
+        data.forEach((item) => {
+          if (item.endsWith('CS')) {
+            csStudents.push(item);
+          }
+          if (item.endsWith('SWE')) {
+            sweStudents.push(item);
+          }
+        });
+        res.write(`Number of students: ${data.length - 1}`);
+
+        res.write(`Number of students in CS: ${csStudents.length}. `);
+        res.write('List: ');
+        csStudents.forEach((student, idx) => {
+          if (idx === (csStudents.length - 1)) {
+            res.write(`${student.split(',')[0]}`);
+          } else {
+            res.write(`${student.split(',')[0]}, `);
+          }
+        });
+      });
+    };
+    read(res);
+    res.end();
+  }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245, '127.0.0.1', () => {
+});
+
+module.exports = app;
--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,11 @@
+const express = require('express');
 
+const port = 1245;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(port, () => {});
