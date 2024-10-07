8161ec4e167800120016fcfa0c30a90c31d32ec5
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-07 10:39:33 +0000

    pasess lint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,24 @@
+const {createServer} = require("node:http");
 
+const port = 1245;
+const host = '127.0.0.1';
+
+const app = createServer((req, res) => {
+    if (req.url == '/') {
+        res.write("Hello Holberton School!");
+    }
+
+    if (req.url == '/students') {
+        res.write("This is the list of our students");
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end("Hello Holberton School!")
+})
+
+app.listen(port, host, () => {
+    process.stdout.write("Hello Holberton School!\n")
+})
+
+module.exports = app;
