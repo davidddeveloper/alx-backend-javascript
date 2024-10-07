290dc9322fa5589121c3813e1b5c0b2409d7ff14
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-07 12:17:03 +0000

    6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,13 @@
-#!/bin/node
 const express = require('express');
 
 const port = 1245;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.status(200).send('Hello Holberton School!');
   res.end();
 });
 
 app.listen(port, () => {});
+
+module.exports = app;
