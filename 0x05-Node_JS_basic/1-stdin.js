b661d2a9f220a9dc4ba4e4206629bdaf049e6088
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-07 10:38:15 +0000

    passes lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdin.on('data', (name) => {
-    console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}`);
 });
 
-console.log(`Welcome to Holberton School, what is your name?`);
+console.log('Welcome to Holberton School, what is your name?');
 
 process.on('SIGINT', () => {
-    console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
