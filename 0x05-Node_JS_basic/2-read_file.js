9c86121243b231c743b994ff84f6da5a050994f3
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-07 11:56:07 +0000

    correctly uses console.log and functional programming

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,41 +5,37 @@ function countStudents(path) {
     let data = fs.readFileSync(path, 'utf-8');
     const sweStudents = [];
     const csStudents = [];
+    const makeString = (list) => {
+      let string = '';
+      list.forEach((item, idx) => {
+        if (idx === (list.length - 1)) {
+          string += item;
+        } else {
+          string += `${item}, `;
+        }
+      });
+
+      return string;
+    };
 
     data = data.split('\n');
     data = data.slice(1);
 
     data.forEach((item) => {
       if (item.endsWith('CS')) {
-        csStudents.push(item);
+        csStudents.push(item.split(',')[0]);
       }
       if (item.endsWith('SWE')) {
-        sweStudents.push(item);
+        sweStudents.push(item.split(',')[0]);
       }
     });
     console.log(`Number of students: ${data.length}`);
 
-    process.stdout.write(`Number of students in CS: ${csStudents.length}. `);
-    process.stdout.write('List: ');
-    csStudents.forEach((student, idx) => {
-      if (idx === (csStudents.length - 1)) {
-        process.stdout.write(`${student.split(',')[0]}`);
-      } else {
-        process.stdout.write(`${student.split(',')[0]}, `);
-      }
-    });
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${makeString(csStudents)}`);
 
-    console.log();
-    process.stdout.write(`Number of students in SWE: ${sweStudents.length}. `);
-    process.stdout.write('List: ');
-    sweStudents.forEach((student, idx) => {
-      if (idx === (sweStudents.length - 1)) {
-        process.stdout.write(`${student.split(',')[0]}`);
-      } else {
-        process.stdout.write(`${student.split(',')[0]}, `);
-      }
-    });
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${makeString(sweStudents)}`);
   } catch (e) {
+    console.log(e);
     throw Error('Cannot load the database');
   }
 }
