2f05678ccb222f9fc62577b0ab6258cdaabdc859
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-07 10:55:56 +0000

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,14 @@
-process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name}`);
-});
-
 console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
+  console.log(`Your name is: ${name}`);
+
+  // console.log('This important software is now closing');
+  process.exit(); // End the process
+});
 
 process.on('SIGINT', () => {
   console.log('This important software is now closing');
+  process.exit();
 });
